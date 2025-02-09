cbafaadde36ddec0a8124854bc8a5d2de643e715
Sergey Bizyulin <servusbizi@gmail.com>
2025-02-09 11:22:31 +0300

    add all needs files

---
+++ src/masks.py
@@ -0,0 +1,29 @@
+from typing import Union
+
+
+# card_number - номер карты
+def get_mask_card_number(card_number: Union[int, str]) -> str:
+    """Функция маскировки номера карты"""
+    card_number_str = str(card_number)
+    if len(card_number_str) != 16:
+        return "Введите 16-и значный номер карты"
+    elif not card_number_str.isdigit():
+        return "Все значения должны быть цифрами"
+    else:
+        return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+
+
+# personal_account - это лицевой счет
+def get_mask_account(personal_account: Union[int, str]) -> str:
+    """Функция маскировки номера счета"""
+    personal_account_str = str(personal_account)
+    if len(personal_account_str) != 6:
+        return "Введите 6-и значный номер лицевого счета"
+    elif not personal_account_str.isdigit():
+        return "Все значения должны быть цифрами"
+    else:
+        return f"**{personal_account_str[2:]}"
+
+
+print(get_mask_card_number(1200334599106670))
+print(get_mask_account(123456))
