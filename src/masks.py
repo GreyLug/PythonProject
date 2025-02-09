be3577b57df60a7f10f5eee24e1945d1be866272
Sergey Bizyulin <servusbizi@gmail.com>
2025-02-09 11:51:49 +0300

    fixes def get_mask_account

--- src/masks.py
+++ src/masks.py
@@ -17,13 +17,13 @@ def get_mask_card_number(card_number: Union[int, str]) -> str:
 def get_mask_account(personal_account: Union[int, str]) -> str:
     """Функция маскировки номера счета"""
     personal_account_str = str(personal_account)
-    if len(personal_account_str) != 6:
-        return "Введите 6-и значный номер лицевого счета"
+    if len(personal_account_str) != 20:
+        return "Введите 20-и значный номер лицевого счета"
     elif not personal_account_str.isdigit():
         return "Все значения должны быть цифрами"
     else:
-        return f"**{personal_account_str[2:]}"
+        return f"**{personal_account_str[-4:]}"
 
 
 print(get_mask_card_number(1200334599106670))
-print(get_mask_account(123456))
+print(get_mask_account(12345678912345678912))
