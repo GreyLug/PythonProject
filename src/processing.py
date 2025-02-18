52121ad863da33719864b61772e4935ea2499067
Sergey Bizyulin <servusbizi@gmail.com>
2025-02-18 17:37:25 +0300

    hot fix

--- src/processing.py
+++ src/processing.py
@@ -1,37 +1,59 @@
-from typing import Dict, List, Union
-
-
-def filter_by_state(banking_transactions: List[Dict[str, Union[str | int]]],
-                    state: str = "EXECUTED") -> List[Dict[str, Union[str | int]]]:
+def filter_by_state(banking_operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функия возврата банковских операций по ключу"""
-    state_banking_transactions = []
-    for item in banking_transactions:
+    new_banking_operations = []
+    for item in banking_operations:
         if item["state"] == state:
-            state_banking_transactions.append(item)
-    return state_banking_transactions
+            new_banking_operations.append(item)
+    return new_banking_operations
 
 
-def sort_by_date(banking_transactions_: List[Dict[str, Union[str | int]]],
-                 reverse: bool = True) -> List[Dict[str, Union[str | int]]]:
+def sort_by_date(banking_transactions: list[dict], sorting_order: bool = True) -> list[dict]:
     """Функция сортировки банковских операций по дате"""
-    data_banking_transactions = sorted(banking_transactions_, key=lambda x: x["date"], reverse=reverse)
-    return data_banking_transactions
+    new_banking_transactions = sorted(
+        banking_transactions, key=lambda dictionary: dictionary["date"], reverse=sorting_order
+    )
+    return new_banking_transactions
 
 
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                      state='CANCELED'))
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], reverse=False))
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        state="CANCELED",
+    )
+)
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ],
+        sorting_order=False,
+    )
+)
